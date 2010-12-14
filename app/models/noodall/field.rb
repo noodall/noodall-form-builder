cf71ff2ad9c40b6c09a4ad3f0b9b59c390d89edf
Steve England <steve@wearebeef.co.uk>
2010-12-14 14:20:26 +0000

    Fix for parameterising field names

--- app/models/noodall/field.rb
+++ app/models/noodall/field.rb
@@ -1,23 +1,23 @@
 module  Noodall
   class Field
     include MongoMapper::EmbeddedDocument
-  
+
     key :_type, String, :required => true
     key :name, String, :required => true
     key :label, String
     key :default, String
     key :required, Boolean, :default => false, :required => true
-  
+
     embedded_in :form
-    
+
     def underscored_name
-      name.parameterize('_').to_s
+      name.parameterize.gsub('-','_').to_s
     end
-    
+
     def default_class(response)
       'default-value' if response.send(underscored_name.to_sym) == default
     end
-  
+
     def mandatory?
       Form::MANDATORY_FIELDS.include?(self.name)
     end
