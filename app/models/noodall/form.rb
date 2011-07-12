67b8273d2e402de7004e04dc31c08faf2fac7722
Alex Heaton <alex@heaton.me>
2011-07-12 11:25:21 +0100

    fix foreign key in forms

--- app/models/noodall/form.rb
+++ app/models/noodall/form.rb
@@ -12,7 +12,7 @@ module Noodall
 
     MANDATORY_FIELDS = ['Name','Email']
     many :fields, :class => Noodall::Field
-    many :responses, :class => Noodall::FormResponse do
+    many :responses, :class => Noodall::FormResponse, :foreign_key => 'noodall_form_id' do
       def ham
         self.select {|r| r.spaminess < (self.class.defensio_config['spam_threshold'] || 0.75)}
       end
