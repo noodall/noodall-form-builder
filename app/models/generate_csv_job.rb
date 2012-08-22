5d13afc18791f7bc283960181b3263ffd16a8535
Jordan Elver <jordan.elver@gmail.com>
2012-08-22 16:43:03 +0100

    Reload the download in case it's been updated
    The email field may of been populated since retrieval

--- app/models/generate_csv_job.rb
+++ app/models/generate_csv_job.rb
@@ -1,11 +1,13 @@
 class GenerateCsvJob
   def self.perform(download_id)
+    Rails.logger.info "Picking up download with ID #{download_id}"
 
     # TODO: Catch errors if download or form do not exist
     download = Noodall::Download.find(download_id)
     form = Noodall::Form.find(download.form_id)
 
     csv = Noodall::FormResponseCsv.new(form, download.conditions).output
+    download.reload
     download.update_attribute(:output, csv)
 
     # Send notification email
